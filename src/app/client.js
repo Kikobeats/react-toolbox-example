cc000a0961096f90176aa03e484811ae7f9fa19a
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-08-18 22:21:39 +0200

    Unify client and App

--- src/app/client.js
+++ src/app/client.js
@@ -1,6 +1,18 @@
 import 'react-toolbox/lib/commons.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import PurpleAppBar from './PurpleAppBar.js';      // AppBar with simple overrides
+import SuccessButton from './SuccessButton.js';    // A button with complex overrides
+import { Button } from 'react-toolbox/lib/button'; // Bundled component import
+
+const App = () => (
+  <div>
+    <PurpleAppBar />
+    <section style={{ padding: 20 }}>
+      <SuccessButton label='Success' primary raised />
+      <Button label='Primary Button' primary />
+    </section>
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('app'));
